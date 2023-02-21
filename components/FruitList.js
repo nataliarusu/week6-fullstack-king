77b797c32f9e7179dd591932de35d112ef87b4a0
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-02-21 10:38:56 +0000

    fruit list comp

--- components/FruitList.js
+++ components/FruitList.js
@@ -1,2 +1,16 @@
-//ul
-//list of all the fruits/products
+import FruitItem from './FruitItem';
+
+export default function FruitList(props) {
+  //array => [{id, title, ...},{}]
+  return props.fruits.map((fruit) => {
+    return (
+      <FruitItem
+        key={fruit.id}
+        title={fruit.title}
+        image={fruit.image}
+        price={fruit.price}
+        link={fruit.link} //generated in pages [fruit_id]
+      />
+    );
+  });
+}
